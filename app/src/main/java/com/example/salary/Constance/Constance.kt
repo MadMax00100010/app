499631accdb9360ede830af2bd127dfc912068d1
Maxim Proskochilov <maxcantn@gmail.com>
2024-09-26 19:06:20 +0300

    коммит всего проекта

---
+++ app/src/main/java/com/example/salary/Constance/Constance.kt
@@ -0,0 +1,20 @@
+package com.example.salary.Constance
+
+object Constance {
+
+        const val INJINER_SALARY = "150 000₽"
+        const val DVORNIK_SALARY = "10 000₽"
+        const val DIRECTOR_SALARY = "450 000₽"
+        const val MEHANIK_SALARY = "50 000₽"
+
+        const val INJINER = "Слава"
+        const val DVORNIK = "Гоша"
+        const val DIRECTOR = "Максим"
+        const val MEHANIK = "Дима"
+
+        const val INJINER_PASSWORD = "3333"
+        const val DVORNIK_PASSWORD = "1111"
+        const val DIRECTOR_PASSWORD = "4444"
+        const val MEHANIK_PASSWORD = "2222"
+
+}
---
+++ app/src/main/java/com/example/salary/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.example.salary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.salary.Constance.Constance
+import com.example.salary.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var bindingClass : ActivityMainBinding
+
+    override fun onCreate(s: Bundle?) {
+
+        super.onCreate(s)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bindingClass.root)
+        bindingClass.imageView.visibility = View.VISIBLE
+
+
+        bindingClass.button.setOnClickListener {
+
+            val User = bindingClass.TT.text.toString()
+
+        when(User) {
+            Constance.DVORNIK -> {
+                bindingClass.textView3.visibility = View.VISIBLE
+                val tempText = "${Constance.DVORNIK_SALARY}"
+                if(bindingClass.edTT.text.toString() == Constance.DVORNIK_PASSWORD){
+
+                    bindingClass.textView3.text = tempText
+                    bindingClass.imageView.setImageResource(R.drawable.frog)
+
+                } else{
+                    bindingClass.textView3.text = "Неверный пароль"
+                    bindingClass.imageView.setImageResource(R.drawable.no_money)
+                }
+            }
+
+            Constance.MEHANIK -> {
+                bindingClass.textView3.visibility = View.VISIBLE
+                val tempText = "${Constance.MEHANIK_SALARY}"
+                if(bindingClass.edTT.text.toString() == Constance.MEHANIK_PASSWORD) {
+
+                    bindingClass.textView3.text = tempText
+                    bindingClass.imageView.setImageResource(R.drawable.pokemon)
+
+                } else{
+                    bindingClass.textView3.text = "Неверный пароль"
+                    bindingClass.imageView.setImageResource(R.drawable.no_money)
+                }
+            }
+
+            Constance.INJINER -> {
+                bindingClass.textView3.visibility = View.VISIBLE
+                val tempText = "${Constance.INJINER_SALARY}"
+                if(bindingClass.edTT.text.toString() == Constance.INJINER_PASSWORD) {
+
+                    bindingClass.textView3.text = tempText
+                    bindingClass.imageView.setImageResource(R.drawable.cat)
+
+                } else{
+                    bindingClass.textView3.text = "Неверный пароль"
+                    bindingClass.imageView.setImageResource(R.drawable.no_money)
+                }
+            }
+
+            Constance.DIRECTOR -> {
+                bindingClass.textView3.visibility = View.VISIBLE
+                val tempText = "${Constance.DIRECTOR_SALARY}"
+                if(bindingClass.edTT.text.toString() == Constance.DIRECTOR_PASSWORD) {
+
+                    bindingClass.textView3.text = tempText
+                    bindingClass.imageView.setImageResource(R.drawable.marmok)
+
+                } else{
+                    bindingClass.textView3.text = "Неверный пароль"
+                    bindingClass.imageView.setImageResource(R.drawable.no_money)
+                }
+            }
+
+            else -> {
+                bindingClass.textView3.visibility = View.VISIBLE
+                bindingClass.textView3.text = "ИДИ РАБОТАЙ!!!"
+                bindingClass.imageView.setImageResource(R.drawable.no_money)
+            }
+        }
+        }
+    }
+}
